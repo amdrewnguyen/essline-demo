66ae0a777a858f477632b724972560e323aad0a1
Andrew Nguyen <drew.nguyen5@gmail.com>
2017-11-15 20:14:55 -0500

    Added s to api address protocol

--- js/demo.js
+++ js/demo.js
@@ -66,7 +66,7 @@ const getWeather = () => {
   return _$.ajax(
     {
       type: 'GET',
-      url: "http://api.openweathermap.org/data/2.5/weather?q=NewYork,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+      url: "https://api.openweathermap.org/data/2.5/weather?q=NewYork,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
       success(data) {
         console.log("We have your weather!");
         console.log(data);
